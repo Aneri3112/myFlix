833baf124011aeb598ac37e09082b32ab1983e27
Aneri3112 <aneri.pate@hotmail.com>
2022-04-07 19:53:22 +0200

    Added endpoints

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express'),
      morgan = require('morgan'),
      bodyParser = require('body-parser');
+     uuid = require('uuid');
 
 const app = express();
 
@@ -53,26 +54,72 @@ let topmovies = [
 
 ];
 
-app.get('/', (req, res) => {
-    res.send('Welcome to myFlix!');
+//Return a list of ALL movies to the user    
+app.get('/movies', (req, res) => {
+    res.status(200).json(topmovies);
 });
 
-app.get('/documentation', (req, res) => {                  
-    res.sendFile('public/documentation.html', { root: __dirname });
-  });
+//Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = topmovies.find(movie => movie.Title === title);
 
-  app.get('/movies', (req, res) => {
-    res.json(topmovies);
-  });
+    if(movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send('no such movie')
+    }
+});
 
-// morgan logs requests
-app.get('/', (req, res) => {
-    res.send('Welcome to my app!');
-  });
-  
-  app.get('/secreturl', (req, res) => {
-    res.send('This is a secret url with super top-secret content.');
-  });  
+//Return data about a genre
+app.get('/movies/genre/genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = topmovies.find(movie => movie.Genre.Name === genreName ).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else
+    res.status(400).send('no such genre')
+});
+
+//Return data about a director
+app.get('/movies/directors/:directorname', (req, res) => {
+    const { directorName } = req.params;
+    const director = topmovies.find(movie => movie.Director.Name === directorName ).Director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else
+    res.status(400).send('no such director')  
+});
+
+//Allow new users to register
+app.post('/users/register', (req, res) => {
+    res.status(201);
+    res.send('User account created');
+});
+
+//Allow users to update their info
+app.put('/users/:user', (req, res) => {
+    res.send('User account updated');
+});
+
+//Allow users to add a movie to their favorites
+app.post('/users/:user/favourites', (req, res) => {
+    res.status(201);
+    res.send('The movie has been add to favorites');
+});
+
+//Allow users to remove a movie from their favorites 
+app.delete('/users/:user/favourites', (req, res) => {
+    res.send('The movie has been removed from favorites');
+});
+
+//Allow existing users to deregister
+app.delete('/users/:user', (req, res) => {
+    res.send('You have been removed')
+});
+
 
 //Static File  
 app.use(express.static('public')); 
