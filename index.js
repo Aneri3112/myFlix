6ea74c28d88e2bed5b8f343957c60e859e9538b0
Aneri3112 <aneri.pate@hotmail.com>
2022-04-07 21:46:24 +0200

    Fix syntax error

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.get('/movies/:title', (req, res) => {
 //Return data about a genre
 app.get('/movies/genre/:genreName', (req, res) => {
     const { genreName } = req.params;
-    const Genre = topmovies.find(movie => movie.genre === genreName ).genre;
+    const Genre = topmovies.find(movie => movie.genre.Name === genreName ).genre;
 
     if (Genre) {
         res.status(200).json(Genre);
