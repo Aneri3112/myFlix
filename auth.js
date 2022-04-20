c659e56cc1ed7a3ae266ecde2f333c3e3ae94ba0
Aneri3112 <aneri.pate@hotmail.com>
2022-04-20 16:52:21 +0200

    Fix syntax error

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const jwtSecret = 'your_jwt_secret';
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
 
 require('./passport');
 
@@ -14,20 +14,20 @@ let generateJWTToken = (user) => {
 
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-      passport.authenticate('local', { session: false }, (error, user, info) => {
-        if (error || !user) {
-          return res.status(400).json({
-            message: 'Something is not right',
-            user: user
-          });
-        }
-        req.login(user, { session: false }, (error) => {
-          if (error) {
-            res.send(error);
-          }
-          let token = generateJWTToken(user.toJSON());
-          return res.json({ user, token }); // this is the same as res.json({ user: user, token: token })
-        });
-      })(req, res);
+        passport.authenticate('local', { session: false }, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is wrong',
+                    user: user
+                });
+            }
+            req.login(user, { session: false }, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        })(req, res);
     });
 }
