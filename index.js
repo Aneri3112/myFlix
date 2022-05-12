a75db470095a406cda36a4e738fdd5710c3dafea
Aneri3112 <aneri.pate@hotmail.com>
2022-05-12 22:00:22 +0200

    Update user detail

--- index.js
+++ index.js
@@ -86,7 +86,19 @@ app.post('/users',
         res.status(500).send('Error ' + err);
       });
 });
-  
+ 
+// gets a user by username
+
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 //UPDATE
 app.put ('/users/:Username',
