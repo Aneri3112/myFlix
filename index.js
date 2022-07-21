afd1bfa0067171fef502e2691f48542624db524d
Aneri3112 <aneri.pate@hotmail.com>
2022-07-22 00:55:26 +0200

    syntax error

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const { check, validationResult } = require('express-validator');
  
 // mongoose.connect('mongodb://localhost:27017/[myFlixDB]', { useNewUrlParser: true, useUnifiedTopology: true });
 
-//mongoose.connect(process.env.CONNECTION_URI || "mongodb://localhost:27017/[myFlixDB]", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI || "mongodb://localhost:27017/[myFlixDB]", { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const morgan = require('morgan'),
    bodyParser = require('body-parser'),
